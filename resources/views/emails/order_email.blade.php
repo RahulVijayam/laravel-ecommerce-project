ffb1a53888c2a9ab8e6a11981506712d17a6b370
Rahul Vijayanagaram <36434065+RahulVijayam@users.noreply.github.com>
2022-07-16 11:53:29 +0530

    Update order_email.blade.php

--- resources/views/emails/order_email.blade.php
+++ resources/views/emails/order_email.blade.php
@@ -12,7 +12,7 @@
                 <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
                     <tr>
                         <td align="center"  style="">
-                            <img src="{{assets('assets/img/Logo.webp')}}"  width="300" height="100" style="display:block;margin-top:20px;"/>
+                            <img src="{{asset('assets/img/Logo.webp')}}"  width="300" height="100" style="display:block;margin-top:20px;"/>
                             
                         </td>
                     </tr>
